2be74203932d8ac6b380a6adbce661a71027a447
BGuga <tmdwns9913@gmail.com>
2024-05-26 19:45:50 +0900

    chore: 주석 설명 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,6 +5,10 @@ const socket = io("https://cloudclap.r-e.kr/", {
 const messageDiv = document.getElementById("message");
 const messageForm = messageDiv.querySelector("form");
 
+/**
+ * 메시지를 송신하는 부분임
+ * message 는 마찬가지로 string, a Blob, an ArrayBuffer, a TypedArray or a DataView object. 형태로 전송 가능함
+ */
 function sendMessage(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
@@ -16,6 +20,10 @@ messageForm.addEventListener("submit", sendMessage);
 
 let roomName;
 let myPeerConnection;
+
+/**
+ * 현재는 1:1 실시간 반영만 우선 개발하고자 하나의 dataChannel을 운영함 만약 여러명 참여해야하는 경우 myDataChannels 로 다수의 Cahnnel을 관리해야함
+ */
 let myDataChannel;
 
 const welcome = document.getElementById("welcome");
@@ -37,8 +45,6 @@ async function handleWelcomeSubmit(event) {
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-// Socket Code
-
 socket.on("welcome", async () => {
   myDataChannel = myPeerConnection.createDataChannel("chat");
   myDataChannel.addEventListener("message", (event) => console.log(event.data));
@@ -49,6 +55,12 @@ socket.on("welcome", async () => {
   socket.emit("offer", offer, roomName);
 });
 
+/**
+ * 중요!! myDataChannel 에 추가하는 myDataChannel.addEventListener 가 데이터를 주고 받는 과정에서 수신 과정에 해당함.
+ * 해당 부분에서 전달 받을 수 있는 형태는 string, a Blob, an ArrayBuffer, a TypedArray or a DataView object. 임
+ * 따라서 데이터를 받은 후 데이터를 정제해서 그림에 포함시키는 과정만 만들면 됨!
+ * 참고 : https://developer.mozilla.org/en-US/docs/Web/API/RTCDataChannel/send
+ **/
 socket.on("offer", async (offer) => {
   myPeerConnection.addEventListener("datachannel", (event) => {
     myDataChannel = event.channel;
@@ -74,7 +86,6 @@ socket.on("ice", (ice) => {
   myPeerConnection.addIceCandidate(ice);
 });
 
-// RTC Code
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection({
     iceServers: [
